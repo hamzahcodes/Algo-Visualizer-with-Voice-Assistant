192922c1fbc020d6187a496fd66049cf4e1e57ee
hamzahcodes <shaikhhamzah93@gmail.com>
2021-05-17 20:39:36 +0530

    added nothing

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+char = '+'
+            # num = re.findall(rf'\b^{char}\d.\b',pattern)
+            # print(num[0])
+
+            # print(num[0], msg, hr , min)
+            # kit.sendwhatmsg(num[0],msg,hr,min)
